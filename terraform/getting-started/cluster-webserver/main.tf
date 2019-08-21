8a04c80c92acafe8a1a4acdf25ef191cacbb63e6
test <test>
2019-08-21 18:57:42 +0530

    Changes to Terraform Web-Server Cluster

--- terraform/getting-started/cluster-webserver/main.tf
+++ terraform/getting-started/cluster-webserver/main.tf
@@ -1,7 +1,83 @@
 
-
 provider "aws" {
   region = "us-east-1"
+}
+
+// ALB 
+resource "aws_lb" "example" {
+  name               = "terraform-asg-example"
+  load_balancer_type = "application"
+  subnets            = data.aws_subnet_ids.default.ids
+  security_groups    = [aws_security_group.alb.id]
+}
+
+// ALB Listener
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.example.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  # By default, return a simple 404 page
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: page not found"
+      status_code  = 404
+    }
+  }
+}
+
+// ALB Listner Rules
+
+resource "aws_lb_listener_rule" "asg" {
+  listener_arn = aws_lb_listener.http.arn
+  priority     = 100
+
+  condition {
+    field  = "path-pattern"
+    values = ["*"]
+  }
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.asg.arn
+  }
+}
+
+
+// Security Group for ALB
+resource "aws_security_group" "alb" {
+  name = "terraform-example-alb"
+
+  # Allow inbound HTTP requests
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow all outbound requests
+  egress {
+    from_port   = "${var.server_port}"
+    to_port     = "${var.server_port}"
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+// Security Group for EC2 
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port   = "${var.server_port}"
+    to_port     = "${var.server_port}"
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 
@@ -38,89 +114,6 @@ resource "aws_autoscaling_group" "example" {
     propagate_at_launch = true
   }
 }
-
-// Security Group for EC2 
-resource "aws_security_group" "instance" {
-  name = "terraform-example-instance"
-
-  ingress {
-    from_port   = ${var.server_port}
-    to_port     = ${var.server_port}
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  lifecycle {
-  create_before_destroy = true
-  }
-}
-
-data "aws_availability_zones" "all" {}
-
-resource "aws_elb" "example" {
-  name               = "terraform-asg-example"
-  availability_zones = ["${data.aws_availability_zones.all.names}"]
-  security_groups    = ["${aws_security_group.elb.id}"]
-
-  listener {
-    lb_port           = 80
-    lb_protocol       = "http"
-    instance_port     = "${var.server_port}"
-    instance_protocol = "http"
-  }
-
-  health_check {
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-    timeout             = 3
-    interval            = 30
-    target              = "HTTP:${var.server_port}/"
-  }
-}
-
-// ALB Listner Rules
-
-resource "aws_lb_listener_rule" "asg" {
-  listener_arn = aws_lb_listener.http.arn
-  priority     = 100
-
-  condition {
-    field  = "path-pattern"
-    values = ["*"]
-  }
-
-  action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.asg.arn
-  }
-}
-
-
-// Security Group for ALB
-resource "aws_security_group" "alb" {
-  name = "terraform-example-alb"
-
-  # Allow inbound HTTP requests
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # Allow all outbound requests
-  egress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-
-
-
-
-
 // Fetch Subnet_Id using data sources and use in ASG
 data "aws_vpc" "default" {
   default = true
@@ -131,7 +124,7 @@ data "aws_subnet_ids" "default" {
 // Target Group for ALB
 resource "aws_lb_target_group" "asg" {
   name     = "terraform-asg-example"
-  port     = var.server_port
+  port     = "${var.server_port}"
   protocol = "HTTP"
   vpc_id   = data.aws_subnet_ids.default.id
   health_check {
@@ -144,4 +137,3 @@ resource "aws_lb_target_group" "asg" {
     unhealthy_threshold = 2
   }
 }
-
